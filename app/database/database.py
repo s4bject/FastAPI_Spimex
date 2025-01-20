c71e96376b8031645413e4008809613b656c0ae1
s4bject <artyon20026@gmail.com>
2025-01-20 15:44:56 +0700

    create database models&engine

---
+++ app/database/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+Base = declarative_base()
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+async_session = sessionmaker(engine, expire_on_commit=False)
---
+++ app/database/models.py
@@ -0,0 +1,20 @@
+from database import Base
+from sqlalchemy import Integer, String, Column, Date, Text
+
+
+class TradingResults(Base):
+    __tablename__ = "spimex_trading_results"
+
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    exchange_product_id = Column(String)
+    exchange_product_name = Column(Text)
+    oil_id = Column(String)
+    delivery_basis_id = Column(Text)
+    delivery_basis_name = Column(String)
+    delivery_type_id = Column(String)
+    volume = Column(String)
+    total = Column(String)
+    count = Column(Integer)
+    date = Column(String)
+    created_on = Column(Date)
+    updated_on = Column(Date)
