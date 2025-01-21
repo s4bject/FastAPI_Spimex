abaf23fd94e72e3cf9e64d04866e3e9e01168f20
s4bject <artyon20026@gmail.com>
2025-01-21 13:21:09 +0700

    add data validator

---
+++ app/shcemas/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import date
+
+
+class TradingResultsResponse(BaseModel):
+    id: int
+    exchange_product_id: str
+    exchange_product_name: str
+    oil_id: str
+    delivery_basis_id: str
+    delivery_basis_name: str
+    delivery_type_id: str
+    volume: str
+    total: str
+    count: int
+    date: str
+    created_on: date
+    updated_on: date
+
+    class Config:
+        orm_mode = True
