f8e462d4b15ff4177e3f804ba97339ed3efd27c2
s4bject <artyon20026@gmail.com>
2025-01-21 13:21:37 +0700

    add new routes and functions

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,16 +1,48 @@
+from datetime import date, datetime
+from typing import Annotated, List, Optional
+
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import Field
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.database import get_db
-from services.spimex_service import get_last_trading_dates
+from services.spimex_service import get_last_trading_dates, get_dynamics, get_trading_results
+
+from shcemas.schemas import TradingResultsResponse
 
 router = APIRouter(prefix="/spimex")
 
 
 @router.get("/last-trading-dates")
-async def get_last_trading_dates_route(nums_days: int = 5, db: AsyncSession = Depends(get_db)):
+async def get_last_trading_dates_route(nums_days: Annotated[int, Field(ge=1)], db: AsyncSession = Depends(get_db)):
     try:
         trading_dates = await get_last_trading_dates(db, nums_days)
         return [date for date in trading_dates]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Ошибка получения данных: {e}")
+
+
+@router.get("/get-dynamics", response_model=List[TradingResultsResponse])
+async def get_dynamics_route(start_date: str,
+                             end_date: str,
+                             oil_id: Optional[str] = None,
+                             delivery_type_id: Optional[str] = None,
+                             delivery_basis_id: Optional[str] = None,
+                             db: AsyncSession = Depends(get_db)):
+    try:
+        start_date_obj = datetime.strptime(start_date, "%Y-%m-%d").date()
+        end_date_obj = datetime.strptime(end_date, "%Y-%m-%d").date()
+        query = await get_dynamics(db, oil_id, delivery_type_id, delivery_basis_id, start_date_obj, end_date_obj)
+        return query
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Ошибка получения данных: {e}")
+
+
+@router.get("/get-trading-results", response_model=List[TradingResultsResponse])
+async def get_trading_results_route(oil_id: Optional[str] = None, delivery_type_id: Optional[str] = None,
+                                    delivery_basis_id: Optional[str] = None, db: AsyncSession = Depends(get_db)):
+    try:
+        query = await get_trading_results(db,oil_id,delivery_type_id,delivery_basis_id)
+        return query
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Ошибка получения данных: {e}")
--- app/services/spimex_service.py
+++ app/services/spimex_service.py
@@ -1,17 +1,53 @@
-from datetime import datetime
+from datetime import date
 from typing import List
 
-from sqlalchemy import select
+from sqlalchemy import select, and_, cast, Date
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.models import TradingResults
 
 
-async def get_last_trading_dates(db: AsyncSession, num_days: int) -> List[datetime]:
+async def get_last_trading_dates(db: AsyncSession, num_days: int) -> List[str]:
     result = await db.execute(
         select(TradingResults.date)
         .distinct()
         .order_by(TradingResults.date.desc())
         .limit(num_days)
     )
+    return result.scalars().all()
+
+
+async def get_dynamics(db: AsyncSession,
+                       oil_id: str,
+                       delivery_type_id: str,
+                       delivery_basis_id: str,
+                       start_date: date,
+                       end_date: date) -> List[TradingResults]:
+    query = select(TradingResults).where(
+        and_(
+            cast(TradingResults.date, Date) >= start_date,
+            cast(TradingResults.date, Date) <= end_date
+        )
+    )
+    if oil_id:
+        query = query.where(TradingResults.oil_id == oil_id)
+    if delivery_type_id:
+        query = query.where(TradingResults.delivery_type_id == delivery_type_id)
+    if delivery_basis_id:
+        query = query.where(TradingResults.delivery_basis_id == delivery_basis_id)
+    result = await db.execute(query)
+    return result.scalars().all()
+
+
+async def get_trading_results(db: AsyncSession, oil_id: str, delivery_type_id: str, delivery_basis_id: str) \
+        -> List[TradingResults]:
+    query = select(TradingResults).order_by(TradingResults.date.desc())
+    query = query.limit(10)
+    if oil_id:
+        query = query.where(TradingResults.oil_id == oil_id)
+    if delivery_type_id:
+        query = query.where(TradingResults.delivery_type_id == delivery_type_id)
+    if delivery_basis_id:
+        query = query.where(TradingResults.delivery_basis_id == delivery_basis_id)
+    result = await db.execute(query)
     return result.scalars().all()
